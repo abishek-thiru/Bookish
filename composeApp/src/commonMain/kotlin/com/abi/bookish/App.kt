9da48d15f2453b5ca0a377908e0085e220e85f38
abishekt <tabishek01@gmail.com>
2025-02-19 16:31:17 -0500

    Koin setup done.ˆ

--- composeApp/src/commonMain/kotlin/com/abi/bookish/App.kt
+++ composeApp/src/commonMain/kotlin/com/abi/bookish/App.kt
@@ -1,28 +1,17 @@
 package com.abi.bookish
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import com.abi.bookish.book.data.network.KtorRemoteBookDataSource
-import com.abi.bookish.book.data.repository.DefaultRepository
 import com.abi.bookish.book.presentation.book_list.BookListScreenRoot
 import com.abi.bookish.book.presentation.book_list.BookListViewModel
-import com.abi.bookish.core.data.HttpClientFactory
-import io.ktor.client.engine.HttpClientEngine
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 @Preview
-fun App(engine: HttpClientEngine) {
+fun App() {
+    val viewModel = koinViewModel<BookListViewModel>()
     BookListScreenRoot(
-        viewModel = remember { BookListViewModel(
-            bookRepository = DefaultRepository(
-                remoteBookDataSource = KtorRemoteBookDataSource(
-                    httpClient = HttpClientFactory.create(
-                        engine
-                    )
-                )
-            )
-        ) },
+        viewModel = viewModel,
         onBookClick = {
 
         }
